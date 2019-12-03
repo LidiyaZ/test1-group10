ae0cb02aa8c47efb0e26dc1af229c42a6c719adb
LidiyaZ <58051223+LidiyaZ@users.noreply.github.com>
2019-12-03 23:59:28 +0300

    Task Eight

---
+++ Task/src/test/java/Eight.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.support.ui.*;
+import org.openqa.selenium.WebElement;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import java.util.List;
+
+
+
+public class Eight {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start () {
+        driver= new ChromeDriver();
+        wait = new WebDriverWait (driver, 10);
+
+    }
+    @Test
+    public void testGoogleSearch() throws InterruptedException {
+
+        driver.get("http://localhost/litecart/en/");
+        //Thread.sleep(2000);
+
+        List<WebElement> productS = driver.findElements(By.cssSelector(".products"));
+        for (int i = 0; i < productS.size(); i++) {
+            List<WebElement> product = productS.get(i).findElements(By.cssSelector(".product"));
+            for (int j = 0; j < product.size(); j++) {
+                List<WebElement> sticker = product.get(j).findElements(By.cssSelector(".sticker"));
+                if (sticker.size() != 1) throw new InterruptedException();
+            }
+        }
+   }
+
+
+    @After
+    public  void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
+
